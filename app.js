b319c9254df4edbd774994957e53e264b8aa3267
Alex Yau <ayau@sas.upenn.edu>
2012-07-15 01:47:10 -0700

    changed port

--- app.js
+++ app.js
@@ -37,6 +37,7 @@ app.get('/programs/:id?', routes.programs);
 //api
 app.get('/api/programs/:id', api.get_program);
 
-app.listen(3000, function(){
+var port = process.env.PORT || 3000;
+app.listen(port, function(){
   console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 });
